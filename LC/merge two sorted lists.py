0b1eb9a4b0dcbd02d969bc4d17395baad5b3a77a
Lysquid <romainbenoit21560@gmail.com>
2022-12-12 21:43:35 +0100

    exos POO

--- LC/merge two sorted lists.py
+++ LC/merge two sorted lists.py
@@ -12,24 +12,16 @@ class ListNode:
 # Partie à coder :
 
 class Solution:
-    
+
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        current = ListNode()
-        if(l2==None and l1==None):
-            current=None
-        elif l1==None :
-            current.val=l2.val
-            
-            current.next=self.mergeTwoLists(l1,l2.next)
-        elif (l2==None or l1.val<l2.val):
-            current.val=l1.val
-            
-            current.next=self.mergeTwoLists(l1.next,l2)
-        
-            
+
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+
+        if l1.val < l2.val:
+            return ListNode(l1.val, self.mergeTwoLists(l1.next, l2))
         else:
-            current.val=l2.val
-            current.next=self.mergeTwoLists(l1,l2.next)
+            return ListNode(l2.val, self.mergeTwoLists(l2.next, l1))           
         
-            
-        return current
