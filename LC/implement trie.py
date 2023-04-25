5bb5bf5e7c366e89fd88806da2222c04287ad5c6
Lysquid <romainbenoit21560@gmail.com>
2023-04-25 12:46:13 +0200

    added tries solutions

---
+++ LC/implement trie.py
@@ -0,0 +1,57 @@
+class Node:
+    
+    def __init__(self):
+        self.children = {}
+        self.word = False
+        
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for car in word:
+            if car not in node.children:
+                node.children[car] = Node()
+            node = node.children[car]
+        node.word = True
+        
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for car in word:
+            if car not in node.children:
+                return False
+            node = node.children[car]
+        return node.word
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for car in prefix:
+            if car not in node.children:
+                return False
+            node = node.children[car]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
